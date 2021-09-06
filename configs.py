53ead94236c0af801b98db2354e91cf472d561ba
Subro <57826642+Subroz@users.noreply.github.com>
2021-09-06 10:39:37 +0600

    Update configs.py

--- configs.py
+++ configs.py
@@ -10,4 +10,4 @@ class Config(object):
     API_ID = "7109229"
     API_HASH = "6a2d24e9e6cc4a371ac3d8eca18b12d7"
     BOT_TOKEN = "1979489937:AAF_XVjkU8B9Hlb_zpvWLRYCW7t-kMwfUpE"
-    USERS = "-557445571"
+    USERS = "-1001410596021"
